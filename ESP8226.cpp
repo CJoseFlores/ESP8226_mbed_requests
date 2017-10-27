b8b867eb52113eb3efafb5dbd3c5ad08c9ec4549
Carlos Flores <cflor083@fiu.edu>
2017-10-27 10:41:24 -0400

    Fixed compile-time errors.

--- ESP8226.cpp
+++ ESP8226.cpp
@@ -1,31 +1,32 @@
 #include "ESP8226.h"
 #include "mbed.h"
+#include <string>
 
 /**
  * Constructs an ESP8226 object.
  */
-ESP8226::ESP8226(PinName rx, PinName tx, int baudRate)
-    : esp(rx, tx)
-{
+ESP8226::ESP8226(PinName rx, PinName tx, int baudRate, Serial *PC) : esp(rx, tx){
     setBaudRate(baudRate);
+    pc = PC; // PLEASE DELETE THIS EVENTUALLY, PASSING THIS AS A REFERENCE TO PRINT!.
 }
 
 /**
  * Resets the chip.
  * @return A message confirming the chip has reset.
  */
-String ESP8226::reset()
+void ESP8226::reset()
 {
-    esp.println("AT+RST"); //Sends RST Command to chip.
-    wait(1);    //Wait for response.
-    return "";
+    esp.printf("AT+RST"); //Sends RST Command to chip.
+    wait(ESP8226_RESET_WAIT);    //Wait for response.
+    getReply(ESP8226_RESET_TIMEOUT);
+    printReply(); //TO-BE-DELETED
 }
 
 /**
  * Connects ESP8226 to specified network with specified credentials.
  * @return A message confirming the chip successfully connected to network.
  */
-String ESP8226::connect(String ssid, String passwd)
+string ESP8226::connect(string ssid, string passwd)
 {
     return "";
 }
@@ -33,14 +34,14 @@ String ESP8226::connect(String ssid, String passwd)
 /**
  * Sends specified data to ESP8226.
  */
-void ESP8226::sendData(String data)
+void ESP8226::sendData(string data)
 {
-    
+    //FILL IN EVENTUALLY
 }
 
 void ESP8226::setBaudRate(int customBaudRate)
 {
-    esp.baud(baudRate);
+    esp.baud(customBaudRate);
 }
 
 //<---Private Methods--->
@@ -50,26 +51,31 @@ void ESP8226::getReply(int timeOut)
 {
     bool buffLimitReached = false; //Used to prevent buffer overflow.
     int count = 0; //Used to iterate through the buffer.
-    
+
     memset(buff, '\0', sizeof(buff)); // Clear current buffer.
-    
-    t1.start() //Start timer
-    
+
+    t1.start(); //Start timer
+
     //Poll Data until buffer is full, or specified timeout is reached.
-    while(!buffLimitReached && t1.read() < timeOut) 
-    {
-        if(count >= ESP8226_BUFF_LIMIT)
-        {
+    while(!buffLimitReached && t1.read() < timeOut) {
+        if(count >= ESP8226_BUFF_LIMIT) {
             buffLimitReached = true;
-        }
-        else if(esp.readable())
-        {
-            buff[count] = esp.getc(); 
-            count++
+        } else if(esp.readable()) {
+            buff[count] = esp.getc();
+            count++;
         }
     }
-    
+
     //Stop and reset timer.
-    t1.stop() 
-    t1.reset()
+    t1.stop();
+    t1.reset();
+}
+
+//MAKE SURE TO DELETE THIS EVENTUALLY, USED FOR DEBUGGING.
+void ESP8226::printReply()
+{
+    string reply(buff);
+    if(reply.find("OK")) {
+        pc->printf("Module Reset\n\r");
+    }
 }
--- ESP8226.h
+++ ESP8226.h
@@ -1,25 +1,32 @@
 #ifndef ESP8226_H
 #define ESP8226_H
 #define ESP8226_BUFF_LIMIT 1024
+#define ESP8226_RESET_WAIT 1
+#define ESP8226_CONNECT_WAIT
+#define ESP8226_RESET_TIMEOUT 5
 
 #include "mbed.h"
+#include <string>
 
-class ESP8226
-{
-    public:
-        ESP8226(PinName rx, PinName tx, int baudRate); //constructor.
-        String reset(); // Resets the chip.
-        String connect(String ssid, String passwd); // Connect to a network.
-        void sendData(); // Writes to ESP8226.
-        void setBaudRate(int baudRate); // Sets a new baudRate.
-    private:
-        //Instance Vars
-        Serial esp; //Used to communicate with ESP8226 through UART.
-        char buff[ESP8226_BUFF_LIMIT]; //Used to grab responses from chip.
-        Timer t1; //Used to provide TimeOut functionality for reply.
-        
-        //Methods
-        void getReply(int timeOut); //Grabs reply from chip.
-}
+class ESP8226 {
+public:
+    ESP8226(PinName rx, PinName tx, int baudRate, Serial *PC); //constructor.
+    void reset(); // Resets the chip.
+    string connect(string ssid, string passwd); // Connect to a network.
+    void sendData(string data); // Writes to ESP8226.
+    void setBaudRate(int baudRate); // Sets a new baudRate.
+private:
+    //Instance Vars
+    Serial esp; //Used to communicate with ESP8226 through UART.
+    char buff[ESP8226_BUFF_LIMIT]; //Used to grab responses from chip.
+    Timer t1; //Used to provide TimeOut functionality for reply.
+
+    Serial *pc;
+
+    //Methods
+    void getReply(int timeOut); //Grabs reply from chip.
+    void printReply();
+
+};
 
 #endif
