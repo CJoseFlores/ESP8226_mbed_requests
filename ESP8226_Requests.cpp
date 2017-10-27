32059f2a48f2b262ee887ef47c79601828aae435
Carlos Flores <cflor083@fiu.edu>
2017-10-27 15:17:25 -0400

    Requests skeleton created.

--- ESP8226_Requests.cpp
+++ ESP8226_Requests.cpp
@@ -1,1 +1,29 @@
-#include "ESP8226_Requests.h"
+#include "ESP8266_Requests.h"
+#include "mbed.h"
+#include <string>
+
+ESP8266_Requests::ESP8266_Requests(PinName rx, PinName tx, int baudRate, string ssid, string passwd ,string server_url, Serial *PC)
+    : esp(rx,tx, baudRate, PC)
+{
+    serverUrl = server_url;
+    //Configuring ESP8226
+    esp.reset();
+    esp.setMode();
+    esp.setConnectionMode();
+    esp.connect(ssid, passwd);
+    esp.getIPInfo();
+}
+
+void ESP8266_Requests::getRequest()
+{
+}
+
+void ESP8266_Requests::postRequest()
+{
+}
+
+//<---Private Methods---->
+void ESP8266_Requests::startTCPSession()
+{
+    
+}
--- ESP8226_Requests.h
+++ ESP8226_Requests.h
@@ -1,19 +1,25 @@
+
 #ifndef ESP8266_Requests_H
 #define ESP8266_Requests_H
 
 #include "mbed.h"
+#include "ESP8226.h"
+#include <string>
 
-public class ESP8266_Requests
+class ESP8266_Requests
 {
     private:
-        String server_url; // Used to connect to RESTFull API server.
-        String endpoint; // Used as REST API endpoint ('/sendsensorvalue')
-        String headers; // Used to HTTP Request headers.
+        string serverUrl; // Used to connect to RESTFull API server.
+        ESP8226 esp; // Communicate with the ESP8226
+        Serial *pc; //Reference to PC to print to console.
+        
+        void startTCPSession(); // Establish the TCP Session with server.
+        
     public:
-}
-
-
-
-
+        ESP8266_Requests(PinName rx, PinName tx, int baudRate , string ssid, string passwd, string server_url, Serial *PC);
+        void getRequest(); // Eventually create this (NOT FOR THE PROJECT).
+        void postRequest();
+        
+};
 
 #endif
